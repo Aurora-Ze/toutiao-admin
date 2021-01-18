73c332fc9304db18c437000cb189f78fbdf122ba
aurora <88775682whz@gmail.com>
2021-01-18 21:01:32 +0800

    用户登录-创建页面组件并配置路由

--- src/router.js
+++ src/router.js
@@ -1,23 +1,19 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import VueRouter from 'vue-router'
+import Login from '@/views/login/index.vue'
+Vue.use(VueRouter)
 
-Vue.use(Router)
+// 路由配置表
+const routes = [
+	{
+		path:'/login',
+		name:'login',
+		component:Login
+	}
+]
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+const router = new VueRouter({
+  routes
 })
+
+export default router
